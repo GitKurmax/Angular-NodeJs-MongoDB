9ef1401d5634be036b749efe59196b12b8422ced
Alex <oleksandr.kurmoiarov@smartexe.com>
2019-10-30 11:16:59 +0200

    Changed environment.url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  url: 'http://localhost:3000/api',
+  url: 'https://still-temple-00170.herokuapp.com/api',
   production: false
 };
 
